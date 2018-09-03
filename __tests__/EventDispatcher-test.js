09d21077488690d657241c9f10a9f31da6440be0
Carlos Alexandre de Assis <carlosalexandre1985@icloud.com>
2018-09-03 13:59:37 -0300

    First version of the unit test.

---
+++ __tests__/EventDispatcher-test.js
@@ -0,0 +1,20 @@
+ import {EventDispatcher} from '../EventDispatcher';
+
+describe('EventDispatcher', () => {
+  let instance = new EventDispatcher();
+
+  //-------------------------------------------
+  // listeners
+  //-------------------------------------------
+  it('listeners', done => {
+    let customMethod = function(e) {
+      expect(e.detail).toBe("param");
+      done();
+    }
+
+    instance.addEventListener("custom", customMethod);
+    // instance.removeEventListener("custom", customMethod); // when uncomented, it generates a timeout bacause no method won't listen to.
+
+    instance.dispatchEvent(new CustomEvent("custom", {detail:"param"}));
+  });
+});
